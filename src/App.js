982bbacbba7fa765e74e7160fc36dbfdf36ba732
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-03-27 09:51:35 -0700

    Add heading h2

--- src/App.js
+++ src/App.js
@@ -1,40 +1,42 @@
 import "./App.css";
 import { useState } from "react";
+import Heading from "./components/Heading";
 
 function App() {
-  [products, setProducts] = useState([
-    {
-      productId: 1,
-      productName: "product1",
-      productOwnerName: "productOwner1",
-      Developers: ["NAME_1", "NAME_2", "NAME_3", "NAME_4", "NAME_5"],
-      scrumMasterName: "scrumMaster1",
-      startDate: "YYYY/MM/DD",
-      methodology: "methodology1",
-    },
-    {
-      productId: 2,
-      productName: "product2",
-      productOwnerName: "productOwner2",
-      Developers: ["NAME_1", "NAME_2", "NAME_3", "NAME_4", "NAME_5"],
-      scrumMasterName: "scrumMaster2",
-      startDate: "YYYY/MM/DD",
-      methodology: "methodology2",
-    },
-    {
-      productId: 3,
-      productName: "product3",
-      productOwnerName: "productOwner3",
-      Developers: ["NAME_1", "NAME_2", "NAME_3", "NAME_4", "NAME_5"],
-      scrumMasterName: "scrumMaster3",
-      startDate: "YYYY/MM/DD",
-      methodology: "methodology3",
-    },
-  ]);
+  // [products, setProducts] = useState([
+  //   {
+  //     productId: 1,
+  //     productName: "product1",
+  //     productOwnerName: "productOwner1",
+  //     Developers: ["NAME_1", "NAME_2", "NAME_3", "NAME_4", "NAME_5"],
+  //     scrumMasterName: "scrumMaster1",
+  //     startDate: "YYYY/MM/DD",
+  //     methodology: "methodology1",
+  //   },
+  //   {
+  //     productId: 2,
+  //     productName: "product2",
+  //     productOwnerName: "productOwner2",
+  //     Developers: ["NAME_1", "NAME_2", "NAME_3", "NAME_4", "NAME_5"],
+  //     scrumMasterName: "scrumMaster2",
+  //     startDate: "YYYY/MM/DD",
+  //     methodology: "methodology2",
+  //   },
+  //   {
+  //     productId: 3,
+  //     productName: "product3",
+  //     productOwnerName: "productOwner3",
+  //     Developers: ["NAME_1", "NAME_2", "NAME_3", "NAME_4", "NAME_5"],
+  //     scrumMasterName: "scrumMaster3",
+  //     startDate: "YYYY/MM/DD",
+  //     methodology: "methodology3",
+  //   },
+  // ]);
 
   return (
     <div className="App">
-      
+      <h1>Product Tracker</h1>
+      <Heading text="Current Products"/>
     </div>
   );
 }
--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-const Heading = () => {
-  return <div></div>;
+const Heading = ({ text }) => {
+  return (
+    <div>
+      <h2>{text}</h2>
+    </div>
+  );
 };
 
 export default Heading;
