8b5c743abbed234428b96f0d40a18d86ad606138
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-03-27 09:45:19 -0700

    Add placeholder objects to products state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  [products, setProducts] = useState([
+    {
+      productId: 1,
+      productName: "product1",
+      productOwnerName: "productOwner1",
+      Developers: ["NAME_1", "NAME_2", "NAME_3", "NAME_4", "NAME_5"],
+      scrumMasterName: "scrumMaster1",
+      startDate: "YYYY/MM/DD",
+      methodology: "methodology1",
+    },
+    {
+      productId: 2,
+      productName: "product2",
+      productOwnerName: "productOwner2",
+      Developers: ["NAME_1", "NAME_2", "NAME_3", "NAME_4", "NAME_5"],
+      scrumMasterName: "scrumMaster2",
+      startDate: "YYYY/MM/DD",
+      methodology: "methodology2",
+    },
+    {
+      productId: 3,
+      productName: "product3",
+      productOwnerName: "productOwner3",
+      Developers: ["NAME_1", "NAME_2", "NAME_3", "NAME_4", "NAME_5"],
+      scrumMasterName: "scrumMaster3",
+      startDate: "YYYY/MM/DD",
+      methodology: "methodology3",
+    },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header">z
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
