fd258a100e9b6311939f661d1f7827b92cf32e29
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-03-28 15:53:20 -0700

    Add data file and import to App.js

--- src/App.js
+++ src/App.js
@@ -2,37 +2,10 @@ import { useState } from "react";
 import Heading from "./components/Heading";
 import Button from "./components/Button";
 import Products from "./components/Products";
+import { data } from "./data";
 
 function App() {
-  const [products, setProducts] = useState([
-    {
-      productId: 1,
-      productName: "product1",
-      productOwnerName: "productOwner1",
-      developers: ["NAME_1", "NAME_2", "NAME_3", "NAME_4", "NAME_5"],
-      scrumMasterName: "scrumMaster1",
-      startDate: "2001/10/02",
-      methodology: "Agile",
-    },
-    {
-      productId: 2,
-      productName: "product2",
-      productOwnerName: "productOwner2",
-      developers: ["NAME_1", "NAME_2", "NAME_3", "NAME_4", "NAME_5"],
-      scrumMasterName: "scrumMaster2",
-      startDate: "2001/10/02",
-      methodology: "Waterfall",
-    },
-    {
-      productId: 3,
-      productName: "product3",
-      productOwnerName: "productOwner3",
-      developers: ["NAME_1", "NAME_2", "NAME_3", "NAME_4", "NAME_5"],
-      scrumMasterName: "scrumMaster3",
-      startDate: "2001/10/02",
-      methodology: "Agile",
-    },
-  ]);
+  const [products, setProducts] = useState(data);
 
   const onSubmit = (newProduct, formType) => {
     if (formType === "create") {
